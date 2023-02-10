65ec792eabcdcef4eed04d979d0105a2296da951
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-02-09 19:33:43 -0700

    learning api use

--- lib/src/Pages/Home.jsx
+++ lib/src/Pages/Home.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom'
 
 export default function Home() {
+    let pokeArr = []
 
     const homeData = useLoaderData()
-    let img = homeData.data.images.small
-    console.log(homeData)
+
+    // homeData.data.map(({images, id }) => {
+    //     return(
+    //         pokeArr.push(
+    //             <div key={id}>
+    //                 <img src={images.small} alt="" />
+    //             </div>
+    //         )
+    //     )
+    // })
+
+    console.log(homeData.data)
 
     return ( 
         <div className="home--container">
-            <img src={img} alt="" />
+            {pokeArr}
         </div>
      );
 }
