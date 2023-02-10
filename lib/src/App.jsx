a1771543363e9c6d720c52e7760734ee9d48a6ab
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-02-09 17:35:04 -0700

    more file setup

--- lib/src/App.jsx
+++ lib/src/App.jsx
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+    Route, 
+    createBrowserRouter, 
+    createRoutesFromElements, 
+    RouterProvider 
+  } from 'react-router-dom'
+import { Home, Layout } from './Pages'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+    </Route>
+  )
+)
 
 export default function App() {
   return (
-    <p>Hello World</p>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ lib/src/Pages/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header, Footer } from './Components'
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return ( 
+        <div className="layout--container">
+            <Header />
+            <div className="layout--content">
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+     );
+}
