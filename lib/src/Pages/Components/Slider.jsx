c117e2e5b0cb392680e0f51d9031d3625e9af9e4
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-02-11 08:13:13 -0700

    setting up props for slider

--- lib/src/Pages/Components/Slider.jsx
+++ lib/src/Pages/Components/Slider.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Slider.css'
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-export default function Slider() {
+export default function Slider({img, name, set, number, setNumber}) {
     return ( 
         <div className="slider--container">
-            <div className="slider--button"></div>
+            <div className="slider--button">{<IoIosArrowBack />}</div>
             <div className="slider--content">
-                <img src='' alt="" />
+                <div className="slider--img__container">
+                    <img src={img} alt="" />
+                </div>
+                
+                <p className="slider--name">{name}</p>
+                <p className="slider--set">{set},{`${number}/${setNumber}`}</p>
             </div>
-            <div className="slider--button"></div>
+            <div className="slider--button">{<IoIosArrowForward />}</div>
         </div>
      );
 }
--- lib/src/Pages/Home.jsx
+++ lib/src/Pages/Home.jsx
@@ -9,23 +9,32 @@ export default function Home() {
     const base1Data = useLoaderData()
     let base1Arr = []
 
-    base1Data.data.map(({images, name, flavorText, number, tcgplayer }) => {
+    base1Data.data.map(({images, name, set, number, tcgplayer }) => {
         return(
             base1Arr.push(
-                // {images},
-                // flavorText,
-                // number,
-                // tcgplayer,
+                {
+                 images,
+                 name,
+                 set,
+                 number,
+                 tcgplayer,
+                }
             )
         )
     })
 
     console.log(base1Data.data)
-    console.log(base1Arr)
+    // console.log(base1Arr)
 
     return ( 
         <div className="home--container">
-            <Slider />
+            <Slider 
+                img={base1Arr[0].images.small} 
+                name={base1Arr[0].name} 
+                set={base1Arr[0].set.name}
+                number={base1Arr[0].number}
+                setNumber={base1Arr[0].set.total}
+            />
         </div>
      );
 }
