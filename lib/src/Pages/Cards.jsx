a34db29fe4473ab4e979d14241e560a73cc1a27d
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-02-13 21:12:25 -0700

    cards data setup & css : fetch is broken from sets

--- lib/src/Pages/Cards.jsx
+++ lib/src/Pages/Cards.jsx
@@ -1,21 +1,43 @@
+import './Cards.css'
 import {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
 
 export default function Cards() {
 
+    let cardSet = []
     let { setId } = useParams()
+
     let url = `https://api.pokemontcg.io/v2/cards?q=id:${setId}`
+    
     const[pkmnData, setPkmnData] = useState()
 
     useEffect(() => {
         fetch(url)
-          .then(response => response.json())
-          .then(data => setPkmnData(data.data))
-      },[])
+            .then(response => response.json())
+            .then(data => {
+                setPkmnData(data.data)
+            })
+    },[])
+      
+    pkmnData.map(({images, name, id}) => {
+        return(
+            cardSet.push(
+                <div className="card--content" key={id}>
+                    <img src={images.small} alt="" />
+                    <p>{name}</p>
+                </div>
+            )
+        )
+    })
     
       console.log(pkmnData)
+      console.log(cardSet)
+    
+    
 
     return ( 
-        <p>cards test</p>
+        <div className='card--container'>
+            {cardSet}
+        </div>
      );
 }
